124018c74b52107d9f47b56a0f127f295e1ab521
Rodrigo <rodrigo14123.rg@gmail.com>
2025-11-28 07:32:23 -0300

    Actualizar configuración de Django para cargar variables de entorno y manejar archivos estáticos en modo de desarrollo

--- blog/manage.py
+++ blog/manage.py
@@ -2,11 +2,18 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from dotenv import load_dotenv
 
 
 def main():
     """Run administrative tasks."""
+
+    load_dotenv()
+
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog.settings')
+
+    PORT=os.getenv('DJANGO_PORT')
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -15,6 +22,11 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+    
+    if 'runserver' in sys.argv and PORT:
+        sys.argv.append(PORT)
+
+
     execute_from_command_line(sys.argv)
 
 
